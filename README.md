96b2ac5fc805daf2dba96d81db4f1702b3429ca2
Kevin Piña <kevin@outlook.com>
2025-06-12 12:30:28 +0200

    With JAAS on EJB

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 JSF - "CDI, Primefaces, JAAS" (jakarta-ee9-ejb-jsf-primefaces-jaas)
 * feature/01_without_jaas
+
+* feature/02_with_jaas_ejb
--- jakarta-ee9-ejb-jsf-primefaces-jaas/src/main/java/com/kevinpina/services/UserServiceImpl.java
+++ jakarta-ee9-ejb-jsf-primefaces-jaas/src/main/java/com/kevinpina/services/UserServiceImpl.java
@@ -13,6 +13,7 @@ import jakarta.inject.Inject;
 import java.util.List;
 
 @Stateless // Defining a EJB and also with this annotation creating a CDI component. Automatically the interface will be @Local by default
+@DeclareRoles({"USER", "ADMIN"})
 public class UserServiceImpl implements  UserService {
 
     @Inject
@@ -21,21 +22,25 @@ public class UserServiceImpl implements  UserService {
     @Inject
     private UserMapper userMapper;
 
+    @PermitAll
     @Override
     public List<UserDTO> listUsers() {
         return userRepository.list().stream().map(userMapper::toDTO).toList();
     }
 
+    @RolesAllowed({"USER", "ADMIN"})
     @Override
     public UserDTO getUser(Long id) {
         return userMapper.toDTO(userRepository.findById(id));
     }
 
+    @RolesAllowed({"ADMIN"})
     @Override
     public void addUser(UserDTO user) {
         userRepository.save(userMapper.toEntity(user));
     }
 
+    @RolesAllowed({"ADMIN"})
     @Override
     public void removeUser(Long id) {
         userRepository.deleteById(id);
