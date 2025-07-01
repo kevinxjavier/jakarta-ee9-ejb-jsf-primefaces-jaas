3a329d7d8014eaebcef1c9e62fc23686b5ee8901
Kevin Piña <kevin@outlook.com>
2025-07-01 17:50:08 +0200

    With JAAS on EJB and JSF. And adding custom login and logout. Using Principal to fetch Username in classes

--- jakarta-ee9-ejb-jsf-primefaces-jaas/src/main/java/com/kevinpina/services/UserServiceImpl.java
+++ jakarta-ee9-ejb-jsf-primefaces-jaas/src/main/java/com/kevinpina/services/UserServiceImpl.java
@@ -4,12 +4,15 @@ import com.kevinpina.entities.UserEntity;
 import com.kevinpina.mappers.UserMapper;
 import com.kevinpina.models.UserDTO;
 import com.kevinpina.repositories.CrudRepository;
+import jakarta.annotation.Resource;
 import jakarta.annotation.security.DeclareRoles;
 import jakarta.annotation.security.PermitAll;
 import jakarta.annotation.security.RolesAllowed;
+import jakarta.ejb.SessionContext;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 
+import java.security.Principal;
 import java.util.List;
 
 @Stateless // Defining a EJB and also with this annotation creating a CDI component. Automatically the interface will be @Local by default
@@ -22,9 +25,25 @@ public class UserServiceImpl implements  UserService {
     @Inject
     private UserMapper userMapper;
 
+    @Resource
+    private SessionContext sessionContext;
+
     @PermitAll
     @Override
     public List<UserDTO> listUsers() {
+        Principal principal = sessionContext.getCallerPrincipal();
+        String username = principal.getName();
+        System.out.println("Username: " + username);
+
+        if (sessionContext.isCallerInRole("ADMIN")) {
+            System.out.println("User role type is ADMIN");
+        } else if (sessionContext.isCallerInRole("USER")) {
+            System.out.println("User role type is USER");
+        } else {
+            System.out.println("User is ANONYMOUS");
+            //throw new SecurityException("User has no access to this page");
+        }
+
         return userRepository.list().stream().map(userMapper::toDTO).toList();
     }
 
